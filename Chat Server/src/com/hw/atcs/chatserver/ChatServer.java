74a1a9ef662ea86f5635b8507ff2887f30e9b9e6
jburns20 <j@burnsfamily.info>
2013-09-09 15:11:43 -0700

    Added main method and constructor

--- Chat Server/src/com/hw/atcs/chatserver/ChatServer.java
+++ Chat Server/src/com/hw/atcs/chatserver/ChatServer.java
@@ -1,6 +1,29 @@
 package com.hw.atcs.chatserver;
 
-public class ChatServer {
+import java.util.LinkedList;
 
+public class ChatServer {
+	public static void main(String[] args) {
+		if (args.length < 1) {
+			System.out.println("Usage: ChatServer <port>");
+			return;
+		}
+		int port;
+		try {
+			port = Integer.parseInt(args[0]);
+		} catch (Exception e) {
+			System.out.println("Usage: ChatServer <port>");
+			return;
+		}
+		new ChatServer(port);
+	}
+	
+	private int port;
+	private LinkedList<UserThread> userThreads;
+	
+	public ChatServer(int port) {
+		this.port = port;
+		this.userThreads = new LinkedList<UserThread>();
+	}
 }
 
