e459bd1f710e2712f5108ac894a82c9eef2fbaa3
Andrew Friedman <afriedman1@hwemail.com>
2013-09-09 15:12:30 -0700

    UserThread Constructor

--- Chat Server/src/com/hw/atcs/chatserver/UserThread.java
+++ Chat Server/src/com/hw/atcs/chatserver/UserThread.java
@@ -1,11 +1,30 @@
 package com.hw.atcs.chatserver;
 
-public class UserThread implements Runnable {
+import java.net.*;
+import java.io.*;
 
-	@Override
+public class UserThread implements Runnable {
+	
+	private String name;
+	protected Socket socket;
+	protected PrintWriter out;
+	protected BufferedReader in;
+
+	public UserThread(Socket s) {
+		socket = s;
+		try {
+            this.out = new PrintWriter(this.socket.getOutputStream(), true);
+            this.in  = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+            
+            /* Say hi to the client */
+            this.out.println("Username: ");
+            name = this.in.readLine();
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+	}
 	public void run() {
 		// TODO Auto-generated method stub
 		
 	}
-
 }
