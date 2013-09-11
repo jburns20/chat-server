8f8f4a0990f712cd218d6028cff7f7fa2d8e9305
Zachary Birnholz <zachary@birnholz.com>
2013-09-11 14:34:44 -0700

    Began ServerSocket functionality

--- Chat Server/src/com/hw/atcs/chatserver/ChatServer.java
+++ Chat Server/src/com/hw/atcs/chatserver/ChatServer.java
@@ -1,6 +1,8 @@
 package com.hw.atcs.chatserver;
 
 import java.util.LinkedList;
+import java.io.IOException;
+import java.net.ServerSocket;
 
 public class ChatServer {
 	public static void main(String[] args) {
@@ -20,10 +22,29 @@ public class ChatServer {
 	
 	private int port;
 	private LinkedList<UserThread> userThreads;
+	private ServerSocket listener;
 	
 	public ChatServer(int port) {
 		this.port = port;
 		this.userThreads = new LinkedList<UserThread>();
+		try {
+			this.listener = new ServerSocket(port);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Error: could not open ServerSocket.");
+			System.exit(1);
+		}
+		//now run the server
+		while(true) {
+			try {
+				UserThread accepted=new UserThread(listener.accept());
+				this.userThreads.addLast(accepted);
+				new Thread(accepted).start();
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.out.println("Could not accept the request.");
+			}
+		}
 	}
 }
 
